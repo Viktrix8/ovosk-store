6205f5fdd3c910c6910fce7c809347152c0e014e
Viktor Sopko <sopko.viktor88@gmail.com>
2022-04-25 20:18:16 +0200

    Fixed security bug.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -166,8 +166,7 @@ const Cart = () => {
                   <div className="" style={{ marginTop: 50 }}>
                     <PayPalButton
                       options={{
-                        clientId:
-                          "AR-_HCaPzzB42AqIAgrE5StYmnsrhiAnWyNzuuzWmppeR0WXznrQZ1wpu1wlJxMIcIVaX2hawvSuM529",
+                        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
                         currency: "EUR",
                       }}
                       onSuccess={() => router.push("/success")}
